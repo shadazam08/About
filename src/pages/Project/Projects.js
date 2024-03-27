873050a9d969b9c2634c6518a48e7e44b5fbcd3d
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2024-03-27 11:20:13 +0530

    Update Projects.js

--- src/pages/Project/Projects.js
+++ src/pages/Project/Projects.js
@@ -26,45 +26,19 @@ const Projects = ({ id }) => {
                             </div>
                         </div>
                     </div>
-                </div>
-                {/* <div className="card">
-                    <div className="face">
-                        <div className='face1'>
-                            <div className="content">
-                                <img src={image1} />
-                                <h3>Code</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="face">
+
+                        <div className="face">
                         <div className='face2'>
                             <div className="content">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <a href="#">Read More</a>
+                                <p style={{ marginTop: '5px' }}>This project is an Meesho Clone Website developed as part of the Newton School SkillBuild.
+                                </p>
+                                <a href="https://github.com/shad-azam/meesho-colone/blob/main/README.md" target='_blank'>Read More</a>
+                                <a href="https://meesho-colone.vercel.app/" target='_blank'>Live Demo</a>
                             </div>
                         </div>
-
                     </div>
                 </div>
-                <div className="card">
-                    <div className="face">
-                        <div className='face1'>
-                            <div className="content">
-                                <img src={image1} />
-                                <h3>Launch</h3>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="face">
-                        <div className='face2'>
-                            <div className="content">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <a href="#">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
+                
             </div>
         </div>
     )
