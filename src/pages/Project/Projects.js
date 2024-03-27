b41aa10b23d7e905be8415b812703d9f5212563b
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2024-03-27 11:27:24 +0530

    Update Projects.js

--- src/pages/Project/Projects.js
+++ src/pages/Project/Projects.js
@@ -22,23 +22,32 @@ const Projects = ({ id }) => {
                                 <p style={{ marginTop: '5px' }}>This project is an Meesho Clone Website developed as part of the Newton School SkillBuild.
                                 </p>
                                 <a href="https://github.com/shad-azam/meesho-colone/blob/main/README.md" target='_blank'>Read More</a>
-                                <a href="https://meesho-colone.vercel.app/" target='_blank'>Live Demo</a>
-                            </div>
-                        </div>
-                    </div>
-
-                        <div className="face">
-                        <div className='face2'>
-                            <div className="content">
-                                <p style={{ marginTop: '5px' }}>This project is an Meesho Clone Website developed as part of the Newton School SkillBuild.
-                                </p>
-                                <a href="https://github.com/shad-azam/meesho-colone/blob/main/README.md" target='_blank'>Read More</a>
-                                <a href="https://meesho-colone.vercel.app/" target='_blank'>Live Demo</a>
+                                <a href="https://onlineshopingwebproject.vercel.app/" target='_blank'>Live Demo</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+
+                <div className="card">
+                    <div className="face">
+                        <div className='face1'>
+                            <div className="content">
+                                <img src={image1} />
+                                <h3>Netflix Clone</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="face">
+                        <div className='face2'>
+                            <div className="content">
+                                <p style={{ marginTop: '5px' }}>This project is an Netflix Clone Website developed as part of the Newton School SkillBuild.
+                                </p>
+                                <a href="https://github.com/shadazam08/netflix-clone/blob/main/README.md" target='_blank'>Read More</a>
+                                <a href="https://netflix-clone-nine-tawny.vercel.app/" target='_blank'>Live Demo</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
